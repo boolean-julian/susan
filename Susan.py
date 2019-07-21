dfd8d359bbedb3f4db3807a026a9014b301a9e34
Julian Lueken <julian.lueken@stud.uni-goettingen.de>
2019-07-21 19:04:17 +0200

    first working example

--- Susan.py
+++ Susan.py
@@ -1,37 +1,50 @@
 from PIL import Image
 import numpy as np
-
+import sys
 
 class Susan:
-	def __init__(self, path):
+	# default mask with 37 neighbors per pixel
+	default_mask = np.matrix([
+		[0,0,1,1,1,0,0],
+		[0,1,1,1,1,1,0],
+		[1,1,1,1,1,1,1],
+		[1,1,1,1,1,1,1],
+		[1,1,1,1,1,1,1],
+		[0,1,1,1,1,1,0],
+		[0,0,1,1,1,0,0]
+	], dtype='?')
+
+	# sets initial mask, file path and comparison function
+	def __init__(self, path, mask = default_mask, compare = "exp"):
 		self.load(path)
+		self._set_mask(mask)
 
-		self.mask = [
-			[0,0,1,1,1,0,0],
-			[0,1,1,1,1,1,0],
-			[1,1,1,1,1,1,1],
-			[1,1,1,1,1,1,1],
-			[1,1,1,1,1,1,1],
-			[0,1,1,1,1,1,0],
-			[0,0,1,1,1,0,0]
-		]
-		self.center = (3,3)
+		self.compare = self._compare_exp
+		if compare == "naive":
+			self.compare = self._compare_naive
+		if compare == "exp":
+			self.compare = self._compare_exp
 
 
+	# setters
 	def _set_path(self, path):
 		self.path = path
 
 	def _set_image(self, img):
 		self.img = img
 
-	def _set_mask(self, mask, center):
-		self.mask = mask
-		self.center = center
-
 	def _set_mask(self, mask):
 		self.mask = mask
-		self.center = (len(mask)//2, len(mask[0])//2)
+		self.center = (self.mask.shape[0]//2, self.mask.shape[1]//2)
 
+		self.nbd_size = mask.sum()
+		if self.nbd_size <= 1:
+			print("Error: Mask should have more than one item (has %d)" % self.nbd_size)
+			sys.exit(0)
+		
+		self._init_nbd()
+
+	# loads image into susan module
 	def load(self, path):
 		self._set_path(path)
 		
@@ -39,15 +52,99 @@ class Susan:
 			self.img = np.array(Image.open(self.path).convert("L"), dtype="i")
 		except:
 			print("Error: Cannot open", self.path)
-			return
+			sys.exit(0)
 
 		self.height = self.img.shape[0]
 		self.width 	= self.img.shape[1]
 
-	def get_nbd(i,j):
-		nbd = []
-		for k in range(len(self.mask)):
-			for l in range(len(self.mask[0])):
-				if self.mask[k][l] == 1:
-					nbd.append((k+i-self.center[0],l+j-self.center[1]))
-		return nbd
+	# get indices from mask
+	def _init_nbd(self):
+		self.mask_nbd = np.array([(-1,-1)]*self.nbd_size, dtype="i4")
+		index = 0
+		for k in range(self.mask.shape[0]):
+			for l in range(self.mask.shape[1]):
+				if self.mask[k,l] == 1:
+					x = k-self.center[0]
+					y = l-self.center[1]
+					self.mask_nbd[index] = (x,y)
+					index += 1
+
+	def _compare_naive(self, a, b, t):
+		if np.abs(self.img[a] - self.img[b]) <= t:
+			return 1
+		return 0
+
+	def _compare_exp(self, a, b, t):
+		return np.exp(-((self.img[a] - self.img[b])/t)**6)
+
+	# n from paper
+	def _nbd_compare(self, i, j, t):
+		s = 0
+		for r in self.mask_nbd:
+			x = i+r[0]
+			y = j+r[1]
+			if x >= 0 and x < self.height and y >= 0 and y < self.width:
+				s += self.compare((i,j), (x,y), t)
+		return s
+	
+	def detect_edges(self, t, filename = "out.png"):
+		r = self.img.copy()
+		max_response = 0
+		g = .75*self.nbd_size
+		for i in range(self.height):
+			for j in range(self.width):
+				r[i,j] = max(0, self.nbd_size - self._nbd_compare(i,j,t))
+				
+				if r[i,j] > max_response:
+					max_response = r[i,j]
+				
+		r = r/max_response * 255
+		self.save(r, filename)
+
+	def save(self, r, filename = "a.png"):
+		"""
+		Saves image referenced in the ConstantDenoiser object.
+		
+		Parameters
+		----------
+		filename: String, optional
+			Path, to which the image will be saved.
+			Will save to ./a.png upon not specifying a String
+
+		"""
+		try:
+			a = Image.fromarray(np.uint8(r))
+			a.save(filename)
+			print("Saved file to", filename)
+		except:
+			print("Error: Couldn't save", filename)
+			return
+
+
+	# debug
+	"""
+	def _nbd_check(self, i, j):
+		A = self._get_nbd(i,j)
+		s = ""
+		for k in range(self.height):
+			for l in range(self.width):
+				if (k,l) in A:
+					s += "0 "
+				else:
+					s += ". "
+			s += "\n"
+
+		t = ""
+		for k in range(self.height):
+			for l in range(self.width):
+				if self.img[k,l] < 127:
+					t += "0 "
+				else:
+					t += ". "
+			t += "\n"
+
+		with open("_check.txt", "w") as text_file:
+			text_file.write("%s" % s)
+			text_file.write("\n")
+			text_file.write("%s" % t)
+	"""
--- test.py
+++ test.py
@@ -1,30 +1,14 @@
-nbd_mask = [
-	[0,0,1,1,1,0,0],
-	[0,1,1,1,1,1,0],
-	[1,1,1,0,1,1,1],
-	[1,1,0,1,0,1,1],
-	[1,1,1,0,1,1,1],
-	[0,1,1,1,1,1,0],
-	[0,0,1,1,1,0,0]
-]
-nbd_center = (3,3)
+from Susan import *
+import numpy as np
 
-def get_nbd(nbd_mask,nbd_center,i,j):
-	nbd = []
-	for i2 in range(len(nbd_mask)):
-		for j2 in range(len(nbd_mask[0])):
-			if nbd_mask[i2][j2] == 1:
-				nbd.append((i2+i-nbd_center[0],j2+j-nbd_center[1]))
-	return nbd
+S = Susan("examples/tux/tux.png")
+"""
+i = 7
+j = 10
+S._nbd_check(i,j)
+print(S._nbd_compare(i,j,5))
+"""
 
-A = get_nbd(nbd_mask, nbd_center, 5,5)
-s = ""
-for i2 in range(11):
-	s += "\n"
-	for j2 in range(11):
-		if (i2,j2) in A:
-			s += "x"
-		else:
-			s += " "
+#print(S.mask_nbd)
 
-print(s)
+S.detect_edges(1)
