efef97650db3458544bc62137173d238fdcb28e8
Julian Lueken <julian.lueken@stud.uni-goettingen.de>
2019-08-19 16:40:58 +0200

    poop

--- python/test.py
+++ python/test.py
@@ -3,5 +3,5 @@ import numpy as np
 
 if __name__ == '__main__':
 	#S = Susan("examples/original/original.png")
-	S = Susan("../examples/lena/lena.png", mask = "mask37", compare = "exp_lut")
+	S = Susan("../examples/tux/tux.png", mask = "mask37", compare = "exp_lut")
 	S.detect_edges_mp(40, filename="out.png", nms = True, heatmap = True)
